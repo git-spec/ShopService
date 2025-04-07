7c5c09707c8ef0ea3c291836d9562f41c43d8122
git-spec <inkyfischer@gmail.com>
2025-04-07 10:24:21 +0200

    feat: Added method getProduct to ProductRepo.

--- src/main/java/org/example/ProductRepo.java
+++ src/main/java/org/example/ProductRepo.java
@@ -3,6 +3,7 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 public class ProductRepo {
     private final List<Product> products;
@@ -26,7 +27,20 @@ public class ProductRepo {
         return Objects.hashCode(getProducts());
     }
 
+    @Override
+    public String toString() {
+        return "ProductRepo{" +
+                "products=" + products +
+                '}';
+    }
+
     public void addProduct(Product product) {
         this.products.add(product);
+    }
+
+    public Optional<Product> getProduct(String id) {
+        Product product = null;
+        for (Product p : products) if (p.id().equals(id)) product = p;
+        return product == null ? Optional.empty() : Optional.of(product);
     }
 }
