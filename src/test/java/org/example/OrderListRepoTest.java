7e852d00076cc566de7237641584a7e3df2c087f
git-spec <inkyfischer@gmail.com>
2025-04-07 13:04:28 +0200

    test: Created test for removeAll of OrderListRepo.

--- src/test/java/org/example/OrderListRepoTest.java
+++ src/test/java/org/example/OrderListRepoTest.java
@@ -32,7 +32,7 @@ class OrderListRepoTest {
     }
 
     @Test
-    void getOrder_shouldHaveNotOrder_whenOrderIsRemoved() {
+    void removeOrder_shouldHaveNotOrder_whenOrderIsRemoved() {
         // GIVEN
         String id = "000001";
         Order order = new Order(
@@ -54,5 +54,41 @@ class OrderListRepoTest {
         Optional<Order> actual = orderList.getOrder(id);
         // THEN
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void removeAll_shouldBeEmpty_whenAllOrdersAreCleared() {
+        // GIVEN
+        String id = "000001";
+        Order order_1 = new Order(
+                "000001",
+                new Product(
+                        "123456",
+                        "Apfel",
+                        0.69,
+                        2
+                ),
+                2,
+                1.38
+        );
+        Order order_2 = new Order(
+                "000002",
+                new Product(
+                        "567890",
+                        "Birne",
+                        0.79,
+                        1
+                ),
+                1,
+                0.79
+        );
+        Optional<Order> expected = Optional.empty();
+        //wHEN
+        OrderListRepo orderList = new OrderListRepo();
+        orderList.addOrder(order_1);
+        orderList.addOrder(order_2);
+        orderList.removeAll();
+        // THEN
+        assertTrue(orderList.getOrders().isEmpty());
     }
 }
