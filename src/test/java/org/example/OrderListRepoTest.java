0bac11f6c026d6f4960171350b207aaf07ebada0
git-spec <inkyfischer@gmail.com>
2025-04-07 12:56:13 +0200

    test: Created test for removeOrder of OrderListRepo.

--- src/test/java/org/example/OrderListRepoTest.java
+++ src/test/java/org/example/OrderListRepoTest.java
@@ -30,4 +30,29 @@ class OrderListRepoTest {
         // THEN
         assertTrue(actual.isPresent());
     }
+
+    @Test
+    void getOrder_shouldHaveNotOrder_whenOrderIsRemoved() {
+        // GIVEN
+        String id = "000001";
+        Order order = new Order(
+                "000001",
+                new Product(
+                        "123456",
+                        "Apfel",
+                        0.69,
+                        2
+                ),
+                2,
+                1.38
+        );
+        Optional<Order> expected = Optional.empty();
+        //wHEN
+        OrderListRepo orderList = new OrderListRepo();
+        orderList.addOrder(order);
+        orderList.removeOrder(id);
+        Optional<Order> actual = orderList.getOrder(id);
+        // THEN
+        assertEquals(expected, actual);
+    }
 }
